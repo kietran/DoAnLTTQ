903f64f5fc763cda2ef1ddc164469843eef3c9e0
Kiệt Trần Quang Anh <22520725@ms.uit.edu.vn>
2023-11-28 09:43:02 +0700

    Visualize Calculator

---
+++ DoAnLTTQ/Form1.cs
@@ -0,0 +1,27 @@
+﻿using ePOSOne.btnProduct;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnLTTQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button_WOC butt = new Button_WOC();
+            
+        }
+    }
+}
