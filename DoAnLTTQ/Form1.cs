f02edc6c0d087639f23c4a818e36a8776e2aaa91
Boygioi15 <115127225+Boygioi15@users.noreply.github.com>
2023-11-29 16:30:05 +0700

    Hoàn thiện structure để viết sự kiện

--- DoAnLTTQ/Form1.cs
+++ DoAnLTTQ/Form1.cs
@@ -12,150 +12,20 @@ namespace DoAnLTTQ1
 {
     public partial class Form1 : Form
     {
+        //state
+        bool onShiftMode, onAlphaMode;
+        int currentCursorPosition;
+
         public Form1()
         {
-            InitializeComponent();
             Control.init(this);
+            InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void customButton11_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void customButton11_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void customButton3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void customButton2_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        /// INSERT BLOCK
         private void NUMB_0_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void button21_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button22_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button23_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button24_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button25_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button26_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button27_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button28_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button29_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button30_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button31_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button32_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button33_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button34_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button35_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button36_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button37_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button38_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button39_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button40_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button41_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button42_Click(object sender, EventArgs e)
-        {
-
+            InputText.Text =  InputText.Text.Insert(currentCursorPosition, "0");
         }
 
         private void MISC_Dot_Click(object sender, EventArgs e)
@@ -173,10 +43,7 @@ namespace DoAnLTTQ1
 
         }
 
-        private void CTRL_Equal_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void OPER_Sub_Click(object sender, EventArgs e)
         {
@@ -227,38 +94,12 @@ namespace DoAnLTTQ1
         {
 
         }
-
-        private void CTRL_Ac_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void CTRL_Del_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void NUMB_9_Click(object sender, EventArgs e)
         {
 
         }
 
         private void NUMB_8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
         {
 
         }
@@ -287,7 +128,24 @@ namespace DoAnLTTQ1
         {
 
         }
+        //CONTROL BLOCK
+        private void InputText_Click(object sender, EventArgs e)
+        {
+            currentCursorPosition = InputText.SelectionStart;
+        }
+        private void CTRL_Equal_Click(object sender, EventArgs e)
+        {
 
+        }
+        private void CTRL_Ac_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CTRL_Del_Click(object sender, EventArgs e)
+        {
+
+        }
         private void label2_Click(object sender, EventArgs e)
         {
 
